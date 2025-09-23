ba07c19f6914bb8d4c621237d7c9a8b8d9311bf9
Diana Sergejeva <diana.sergejeva@ivkhk.ee>
2025-09-23 20:01:13 +0300

    обновление 23.09

---
+++ function.js
@@ -0,0 +1,117 @@
+// Основные способы объявления функций
+
+console.log("---1. Function declaration---");
+// можно вызывать до объявления функции, так как оно поднимается в начало области видимости
+console.log(sum(5, 3));
+function sum(a, b) {
+    return a + b;
+}
+
+console.log("---2. Function Expression");
+// нельзя вызывать до объявления.
+// Функция создаётся, когда до этого места доходит обработчик кода
+// console.log(sumExpression(9, 15)); так нельзя делать!!
+const sumExpression = function (a, b) {
+    return a + b;
+};
+
+console.log(sumExpression(9, 15));
+// функция также не поднимается
+// главная особенность - не имеет своего контекста "this"
+console.log("---3. Стрелочная функция-----");
+
+const sumArrow = (a, b) => {
+    return a + b;
+};
+
+console.log(sumArrow(3, 7));
+
+// короткий синтаксис
+const multiply = (a, b) => a * b;
+console.log(multiply(5, 7));
+
+//Пример с "this"
+const person = {
+    name: "Igor",
+    greetRegular: function() {
+        //this - здесь это объект person
+        console.log(`Hello from ${this.name} (regular)`);
+    }, 
+    greetArrow: () => {
+        //У стрелочной функции нет своего this (контекста)
+        console.log(`Hello from ${this.name} arrow`);
+    }
+}
+person.greetRegular();
+person.greetArrow();
+
+//ПАРАМЕТРЫ ФУЕКЦИЙ
+
+console.log("-----Параметры фуекций-----");
+
+//Параметры по-умолчанию (default params)
+function greet(name = "Somebody") {
+    console.log(`Welcome ${name}!`);
+}
+greet("Anna");
+greet();
+
+//REST-params
+//Собирают все переданные аргументы (параметры) в массив
+function sumAll (...numbers) {
+    let total = 0;
+    for (const num of numbers) {
+        total += num;
+    }
+    return total;
+}
+console.log("Summa 1,2,3,4: ", sumAll(1,2,3,4));
+
+//call-back - функции
+//это функция, переданная в другую функцию
+//в качестве аргумента
+
+//классический пример callback
+setTimeout(() => {
+    console.log("это сообщение появится через 2 сек");
+}, 2000);
+
+function operate (a, b, operationCallback) {
+    return operationCallback(a,b);
+}
+const add = (x,y) => x + y;
+const subtract = (x,y) => x - y;
+console.log("Вызов колбеком add: ", operate(10,5, add));
+console.log("Вызов колбеком su: ", operate(10,5, subtract));
+
+console.log("-----Замыкания-----");
+//Замыкания - комбинация функции и ее окружения.
+//проще - внутренняя функция "помнит" переменные
+//внешней функции даже после того,
+//как внешняя завершила свою работу.
+
+function createCounter() {
+    let count = 0;  //эта переменная в лексич.окружении
+
+    //внутренняя функция, которая будет возвращена
+    return function() {
+        count++;
+        console.log(count);
+        return count;
+    };
+}
+const counter1 = createCounter();
+counter1();
+counter1();  //помнит предыдущее значение.
+
+const counter2 = createCounter();
+counter2();
+
+console.log("-----Смовызывающаяся функция------");
+//IIFE - Immediately Invoked Function Expression
+
+(function() {
+    const secretMessage = "Это сообщение не доступно глобально, только внутри этой функции";
+    console.log(secretMessage);
+})();
+
--- oop.js
+++ oop.js
@@ -1,77 +1,116 @@
-//ОПП - парадигма программирования
-//которая использует "объекты" для представления
-//данных и методов для работы с этими данными.
+// парадигма програмирования, которая использует "объекты" для представления данных
+// и методов для работы с этими данными.
 
-//1. Класс и Объект (Classes & Object)
+//1. Класс и Объект (Class & Object)
 //класс - "чертёж" для создания объектов
 //объект - экземпляр класса с собственными данными
-// object - instance of class
+//object - instance of class
 
 class Animal {
-    //конструктор - специальный метод
-    //для создания и инициализации объекта
-    //spesial method for creating and
-    //initializing an object created with a class
-
+    // конструктор = специальный метод
+    // для создания и инициализации объекта
     constructor(name, age) {
-        //this - ссылка на текущий объект
-        //reference to the current object
+        // this - ссылка на текущий объект
         this.name = name;
         this.age = age;
     }
-    //метод - это функция внутри объекта
-    //method - a function inside an object
+    // метод - это функция внутри объекта
     speak() {
-        console.log(`${this.name} издаёт какой-то звук.`);
+        console.log(`${this.name} издаёт какой то звук.`);
     }
     displayInfo() {
         console.log(`Имя: ${this.name}, Возраст: ${this.age}`);
     }
 }
-
-//создание объектов (экземпляров класса)
-//creating objects (instaces of a class)
-const genericAnimal = new Animal(`Some animal`, 5);
+// создание объектов (экземпляров класса)
+const genericAnimal = new Animal('Some animal', 5);
 const myDog = new Animal ("Шарик", 8);
-
 genericAnimal.displayInfo();
 myDog.displayInfo();
 myDog.speak();
 
-//2. Инкапсуляция (Encapsulation)
-//инкапсуляция - скрытие внутренней реализации
-//encapsulation - hiding internal implementation
-//или же - объединение данных и методов
-//и работы с этими данными в одном классе
-
-console.log(`----Инкапсуляция----`);
-
+// Инкапсуляция
+// инкапсуляция - скрытие внутренней реализации от внешнего мира
+// или же - объединение данных и методов
+// и работы с этими данными в одном классе
+console.log('---Инкапсуляция---');
 class BankAccount {
-    //# - означает, что свойство приватное
-    //private property
-
-    #balance =0; //приватное свойство (скрытое)
-    //private property
+    //# означает, что свойство приватное
+    #balance = 0; //приватное свойство
     constructor(initialBalance) {
         if(initialBalance > 0) {
-            this.#balance = initialBalance;
+            this.#balance = initialBalance
         }
     }
-    //публичный метод для внесения денег
-    //publicmethod for depositing money
-    deposit(amount) {if(amount > 0){
+    // публичный метод для внесения денег
+    deposit(amount) {
         this.#balance += amount;
         console.log(`Внесено: ${amount}. Новый баланс: ${this.#balance}`);
+    } 
+    // публичный метод для снятия денег
+    withdraw(amount) {
+        if(amount > 0 && amount <= this.#balance) {
+            this.#balance -= amount;
+            console.log(`Снято: ${amount}. Новый баланс:${this.#balance}`);
+        } else {
+            console.log('Недостаточно средств или неверная сумма');
+        }
+    }
+
+
+    // приватный метод для получения баланса
+    #getBalance() {
+        return this.#balance;
+
+    
+}
+}
+const myAccount = new BankAccount(100);
+myAccount.deposit(50);
+myAccount.withdraw(30);
+// console.log(myAccount.#balance); Ошибка: приватное свойство
+// myAccount.#getBalance; //  Ошибка: приватный метод
+
+// 3. Наследование (Inheritance)
+// наследование -- создание нового класса на основе существующего
+
+console.log('---Наследование---');
+
+class Dog extends Animal {
+    constructor(name, age, breed) {
+        // super - вызов конструктора родительского класса
+        super(name, age);
+        this.breed = breed; // добавляем своё, новое свойство
+    }
+    // переопределение метода speak
+    speak() {
+        console.log(`${this.name} лает: гав-гав.`);
+    }
+
+    // Можно вызывать и родительский метод внутри дочернего
+    displayInfo() {
+        super.displayInfo(); //родительский
+        console.log(`Порода: ${this.breed}`);
     }
 }
-//публичный метод для снятия денег
-//public method for withdrawing money
-withdraw(amount) {
-    if(amount > && amount <= this.#balance) {
-        this.#balance -= amount;
-        console.log(`Снято: ${amount}. Новый баланс: ${this.#balance}`);
-    } else {
-        console.log(`Недостаточно средств или неверная сумма.`);
+
+const newDog = new Dog ("Бобик", 5, "Овчарка");
+newDog.displayInfo(); // вызывает оба метода: родительский и дочерний.
+newDog.speak(); // Вызовет переопределённый метод
+
+// 4. Полиморфизм
+console.log("---Полиморфизм---");
+class Cat extends Animal {
+    // переопределим метод speak.
+    speak() {
+        console.log(`${this.name} мяукает: Мяуу!`);
     }
 }
-}
+const myCat = new Cat("Васька", 2);
+// Создадим массив из разных объектов и вызовем один и тот же метод
+const animals = [myCat, newDog, new Animal("Нечто", 1)];
+// Проходим по массиву и вызываем один и тот же метов speak()
+// в зависимости от класса объекта будет вызвана своя реализация этого метода
+animals.forEach(animal => {
+animal.speak();
+}); 
